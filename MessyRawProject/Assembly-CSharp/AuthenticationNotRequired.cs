fdcd18ddefadc987fef3fa06d5578a571c366419
NukedBart <oxbilibiliox@gmail.com>
2022-06-01 02:07:57 +0800

    removed useless classes #1

--- MessyRawProject/Assembly-CSharp/AuthenticationNotRequired.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-// Token: 0x0200031A RID: 794
-public interface AuthenticationNotRequired
-{
-}
--- MessyRawProject/Assembly-CSharp/BattleRedirectMessage.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-// Token: 0x020002BA RID: 698
-public class BattleRedirectMessage : RedirectConnection
-{
-}
