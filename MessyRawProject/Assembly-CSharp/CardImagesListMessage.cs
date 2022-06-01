51b3df815ff4e4c045e9aee55585e2f634e19a9d
NukedBart <oxbilibiliox@gmail.com>
2022-06-01 16:20:45 +0800

    removed useless classes, moved to NativeClassImplementation

--- MessyRawProject/Assembly-CSharp/CardImagesListMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-// Token: 0x02000299 RID: 665
-public class CardImagesListMessage : Message, AuthenticationNotRequired
-{
-	// Token: 0x04000F28 RID: 3880
-	public long[] images;
-}
