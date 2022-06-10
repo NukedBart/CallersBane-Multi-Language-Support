4c1cc0579934b568fd27bc83ba52efd9c47f249e
NukedBart <oxbilibiliox@gmail.com>
2022-06-11 06:09:01 +0800

    merged changed by Stijn

--- MessyRawProject/Assembly-CSharp/Stijn/Localization/LocalizationManager.cs
+++ MessyRawProject/Assembly-CSharp/Stijn/Localization/LocalizationManager.cs
@@ -11,37 +11,37 @@ namespace Stijn.Localization
 	public static class LocalizationManager
 	{
 		// Token: 0x1400000A RID: 10
-		// (add) Token: 0x06001A3D RID: 6717 RVA: 0x00097790 File Offset: 0x00095990
-		// (remove) Token: 0x06001A3E RID: 6718 RVA: 0x000977C4 File Offset: 0x000959C4
+		// (add) Token: 0x06001A4A RID: 6730 RVA: 0x0009816C File Offset: 0x0009636C
+		// (remove) Token: 0x06001A4B RID: 6731 RVA: 0x000981A0 File Offset: 0x000963A0
 		public static event LocalizationManager.LocalizationLoadedDelegate LocalizationLoadedEvent;
 
 		// Token: 0x1400000B RID: 11
-		// (add) Token: 0x06001A3F RID: 6719 RVA: 0x000977F8 File Offset: 0x000959F8
-		// (remove) Token: 0x06001A40 RID: 6720 RVA: 0x0009782C File Offset: 0x00095A2C
+		// (add) Token: 0x06001A4C RID: 6732 RVA: 0x000981D4 File Offset: 0x000963D4
+		// (remove) Token: 0x06001A4D RID: 6733 RVA: 0x00098208 File Offset: 0x00096408
 		public static event LocalizationManager.LocalizationLoadedDelegate LocalizationLoadedFromGoogleSheetsEvent;
 
 		// Token: 0x1400000C RID: 12
-		// (add) Token: 0x06001A41 RID: 6721 RVA: 0x00097860 File Offset: 0x00095A60
-		// (remove) Token: 0x06001A42 RID: 6722 RVA: 0x00097894 File Offset: 0x00095A94
+		// (add) Token: 0x06001A4E RID: 6734 RVA: 0x0009823C File Offset: 0x0009643C
+		// (remove) Token: 0x06001A4F RID: 6735 RVA: 0x00098270 File Offset: 0x00096470
 		public static event LocalizationManager.LanguageDelegate LanguageChangedEvent;
 
 		// Token: 0x1400000D RID: 13
-		// (add) Token: 0x06001A43 RID: 6723 RVA: 0x000978C8 File Offset: 0x00095AC8
-		// (remove) Token: 0x06001A44 RID: 6724 RVA: 0x000978FC File Offset: 0x00095AFC
+		// (add) Token: 0x06001A50 RID: 6736 RVA: 0x000982A4 File Offset: 0x000964A4
+		// (remove) Token: 0x06001A51 RID: 6737 RVA: 0x000982D8 File Offset: 0x000964D8
 		public static event LocalizationManager.TokenDelegate TokenChangedEvent;
 
-		// Token: 0x06001A45 RID: 6725 RVA: 0x00012E94 File Offset: 0x00011094
+		// Token: 0x06001A52 RID: 6738 RVA: 0x00012F25 File Offset: 0x00011125
 		public static void Initialize()
 		{
 			LocalizationManager.LoadLocalizationData();
 		}
 
-		// Token: 0x06001A46 RID: 6726 RVA: 0x000028DF File Offset: 0x00000ADF
+		// Token: 0x06001A53 RID: 6739 RVA: 0x000028DF File Offset: 0x00000ADF
 		public static void Deinitialize()
 		{
 		}
 
-		// Token: 0x06001A47 RID: 6727 RVA: 0x00012E9B File Offset: 0x0001109B
+		// Token: 0x06001A54 RID: 6740 RVA: 0x00012F2C File Offset: 0x0001112C
 		public static void AddText(string key, List<string> translations)
 		{
 			if (LocalizationManager.s_Data != null)
@@ -50,18 +50,7 @@ namespace Stijn.Localization
 			}
 		}
 
-		// Token: 0x06001A48 RID: 6728 RVA: 0x00012EB1 File Offset: 0x000110B1
-		public static void SetLanguage(LocalizationManager.Language language)
-		{
-			LocalizationManager.s_Language = language;
-			string text = LocalizationManager.s_LanguageCultures[(int)language];
-			if (LocalizationManager.LanguageChangedEvent != null)
-			{
-				LocalizationManager.LanguageChangedEvent(language);
-			}
-		}
-
-		// Token: 0x06001A49 RID: 6729 RVA: 0x00097930 File Offset: 0x00095B30
+		// Token: 0x06001A55 RID: 6741 RVA: 0x0009830C File Offset: 0x0009650C
 		public static void SetCustomToken(string key, string value)
 		{
 			if (LocalizationManager.s_CustomTokens == null)
@@ -82,7 +71,7 @@ namespace Stijn.Localization
 			}
 		}
 
-		// Token: 0x06001A4A RID: 6730 RVA: 0x00012ED3 File Offset: 0x000110D3
+		// Token: 0x06001A56 RID: 6742 RVA: 0x00012F42 File Offset: 0x00011142
 		public static void RemoveCustomToken(string key)
 		{
 			if (LocalizationManager.s_CustomTokens == null)
@@ -100,35 +89,14 @@ namespace Stijn.Localization
 			}
 		}
 
-		// Token: 0x06001A4B RID: 6731 RVA: 0x00012F11 File Offset: 0x00011111
-		public static LocalizationManager.Language GetLanguage()
-		{
-			return LocalizationManager.s_Language;
-		}
-
-		// Token: 0x06001A4C RID: 6732 RVA: 0x00012F18 File Offset: 0x00011118
+		// Token: 0x06001A57 RID: 6743 RVA: 0x00012F80 File Offset: 0x00011180
 		public static string GetText(string key, params object[] customTokens)
 		{
+			LocalizationManager.LoadLocalizationData();
 			return LocalizationManager.GetText(key, LocalizationManager.GetLanguage(), customTokens);
 		}
 
-		// Token: 0x06001A4D RID: 6733 RVA: 0x00097988 File Offset: 0x00095B88
-		public static string GetText(string key, LocalizationManager.Language language, params object[] customTokens)
-		{
-			if (key == null)
-			{
-				return "NO KEY FOUND";
-			}
-			key = key.Trim();
-			LocalizationManager.LoadLocalizationData();
-			string text = string.Empty;
-			LocalizationManager.s_Data.GetText(key, language, out text);
-			text = Regex.Unescape(text);
-			text = LocalizationManager.ReplaceTokens(text, customTokens);
-			return text;
-		}
-
-		// Token: 0x06001A4E RID: 6734 RVA: 0x000979D0 File Offset: 0x00095BD0
+		// Token: 0x06001A58 RID: 6744 RVA: 0x00098364 File Offset: 0x00096564
 		private static string ReplaceTokens(string text, params object[] customTokens)
 		{
 			if (customTokens == null || customTokens.Length == 0)
@@ -190,7 +158,7 @@ namespace Stijn.Localization
 			return stringBuilder.ToString();
 		}
 
-		// Token: 0x06001A4F RID: 6735 RVA: 0x00097B90 File Offset: 0x00095D90
+		// Token: 0x06001A59 RID: 6745 RVA: 0x00098524 File Offset: 0x00096724
 		private static string ReplaceCurlyBraceTokens(string text, params object[] customTokens)
 		{
 			bool flag = false;
@@ -248,14 +216,85 @@ namespace Stijn.Localization
 			return text;
 		}
 
-		// Token: 0x06001A50 RID: 6736 RVA: 0x00012F26 File Offset: 0x00011126
+		// Token: 0x06001A5A RID: 6746 RVA: 0x00012F93 File Offset: 0x00011193
 		public static bool IsTextAvailable(string key)
 		{
 			return LocalizationManager.IsTextAvailable(key, LocalizationManager.s_Language);
 		}
 
-		// Token: 0x06001A51 RID: 6737 RVA: 0x00097CC8 File Offset: 0x00095EC8
-		public static bool IsTextAvailable(string key, LocalizationManager.Language language)
+		// Token: 0x06001A5B RID: 6747 RVA: 0x00012FA0 File Offset: 0x000111A0
+		private static void LoadLocalizationData()
+		{
+			if (LocalizationManager.s_Data == null)
+			{
+				LocalizationManager.s_Data = new LocalizationData();
+				LocalizationManager.DeserializeFromFolder();
+				LocalizationManager.SetLanguage("CHINESE");
+			}
+		}
+
+		// Token: 0x06001A5C RID: 6748 RVA: 0x00012FC3 File Offset: 0x000111C3
+		static LocalizationManager()
+		{
+			LocalizationManager.s_Data = null;
+		}
+
+		// Token: 0x06001A5D RID: 6749 RVA: 0x00012FF6 File Offset: 0x000111F6
+		public static void SetLanguage(string languageKey)
+		{
+			if (LocalizationManager.s_Data != null)
+			{
+				LocalizationManager.SetLanguage(LocalizationManager.s_Data.GetLanguage(languageKey));
+			}
+		}
+
+		// Token: 0x06001A5E RID: 6750 RVA: 0x0001300F File Offset: 0x0001120F
+		public static void SetLanguage(int languageID)
+		{
+			if (LocalizationManager.s_Data != null)
+			{
+				LocalizationManager.SetLanguage(LocalizationManager.s_Data.GetLanguage(languageID));
+			}
+		}
+
+		// Token: 0x06001A5F RID: 6751 RVA: 0x00013028 File Offset: 0x00011228
+		private static void SetLanguage(LocalizationLanguage language)
+		{
+			if (language == null)
+			{
+				return;
+			}
+			LocalizationManager.s_Language = language;
+			if (LocalizationManager.LanguageChangedEvent != null)
+			{
+				LocalizationManager.LanguageChangedEvent(language);
+			}
+		}
+
+		// Token: 0x06001A60 RID: 6752 RVA: 0x00013046 File Offset: 0x00011246
+		public static LocalizationLanguage GetLanguage()
+		{
+			return LocalizationManager.s_Language;
+		}
+
+		// Token: 0x06001A61 RID: 6753 RVA: 0x0009865C File Offset: 0x0009685C
+		public static string GetText(string key, LocalizationLanguage language, params object[] customTokens)
+		{
+			if (key == null)
+			{
+				return "NO KEY FOUND";
+			}
+			key = key.Trim();
+			LocalizationManager.LoadLocalizationData();
+			string text = string.Empty;
+			LocalizationManager.s_Data.GetText(key, language, out text);
+			text = Regex.Unescape(text);
+			text = LocalizationManager.ReplaceTokens(text, customTokens);
+			return text;
+		}
+
+		// Token: 0x06001A62 RID: 6754 RVA: 0x000986A4 File Offset: 0x000968A4
+		public static bool IsTextAvailable(string key, LocalizationLanguage language)
 		{
 			if (string.IsNullOrEmpty(key))
 			{
@@ -270,24 +309,22 @@ namespace Stijn.Localization
 			return LocalizationManager.s_Data.GetText(key, language, out empty);
 		}
 
-		// Token: 0x06001A52 RID: 6738 RVA: 0x00012F33 File Offset: 0x00011133
-		private static void LoadLocalizationData()
+		// Token: 0x06001A63 RID: 6755 RVA: 0x0001304D File Offset: 0x0001124D
+		public static void ClearLocalizationData()
 		{
-			if (LocalizationManager.s_Data == null)
-			{
-				LocalizationManager.s_Data = new LocalizationData();
-				LocalizationManager.DeserializeFromFile();
-			}
+			LocalizationManager.LoadLocalizationData();
+			LocalizationManager.s_Data.Clear();
 		}
 
-		// Token: 0x06001A53 RID: 6739 RVA: 0x00097D04 File Offset: 0x00095F04
-		public static bool DeserializeFromFile()
+		// Token: 0x06001A64 RID: 6756 RVA: 0x000986E0 File Offset: 0x000968E0
+		public static bool DeserializeFromFile(bool json)
 		{
-			string dataFilePath = LocalizationManager.s_Data.DataFilePath;
+			LocalizationManager.LoadLocalizationData();
+			string dataPath = LocalizationManager.s_Data.DataPath;
 			string fileText = string.Empty;
 			try
 			{
-				fileText = File.ReadAllText(dataFilePath, Encoding.UTF8);
+				fileText = File.ReadAllText(dataPath, Encoding.UTF8);
 			}
 			catch (Exception ex)
 			{
@@ -298,8 +335,15 @@ namespace Stijn.Localization
 				}
 				return false;
 			}
-			LocalizationManager.LoadLocalizationData();
-			bool flag = LocalizationManager.s_Data.Deserialize(fileText);
+			bool flag;
+			if (!json)
+			{
+				flag = LocalizationManager.s_Data.DeserializeFromCSV(fileText);
+			}
+			else
+			{
+				flag = LocalizationManager.s_Data.DeserializeFromJSON(fileText);
+			}
 			if (LocalizationManager.LocalizationLoadedEvent != null)
 			{
 				LocalizationManager.LocalizationLoadedEvent(flag);
@@ -307,70 +351,83 @@ namespace Stijn.Localization
 			return flag;
 		}
 
-		// Token: 0x040015E8 RID: 5608
-		private static string[] s_LanguageCultures = new string[]
+		// Token: 0x06001A65 RID: 6757 RVA: 0x00098774 File Offset: 0x00096974
+		public static bool DeserializeFromFolder()
 		{
-			"en-GB",
-			"nl-NL",
-			"de-DE",
-			"zh-CN"
-		};
+			LocalizationManager.LoadLocalizationData();
+			bool flag = true;
+			string dataPath = LocalizationManager.s_Data.DataPath;
+			DirectoryInfo directoryInfo = new DirectoryInfo(dataPath);
+			if (directoryInfo == null || !directoryInfo.Exists)
+			{
+				directoryInfo = new FileInfo(dataPath).Directory;
+				if (directoryInfo == null || !directoryInfo.Exists)
+				{
+					return false;
+				}
+			}
+			FileInfo[] files = directoryInfo.GetFiles("*.json");
+			if (files == null)
+			{
+				return false;
+			}
+			foreach (FileInfo fileInfo in files)
+			{
+				if (fileInfo != null && flag)
+				{
+					string fileText = string.Empty;
+					try
+					{
+						fileText = File.ReadAllText(fileInfo.FullName, Encoding.UTF8);
+					}
+					catch (Exception ex)
+					{
+						Debug.LogError(ex.Message);
+						if (LocalizationManager.LocalizationLoadedEvent != null)
+						{
+							LocalizationManager.LocalizationLoadedEvent(false);
+						}
+						return false;
+					}
+					flag = LocalizationManager.s_Data.DeserializeFromJSON(fileText);
+				}
+			}
+			if (LocalizationManager.LocalizationLoadedEvent != null)
+			{
+				LocalizationManager.LocalizationLoadedEvent(flag);
+			}
+			return flag;
+		}
 
-		// Token: 0x040015E9 RID: 5609
+		// Token: 0x040015ED RID: 5613
 		private static string[] s_TokenBrackets = new string[]
 		{
 			"[",
 			"]"
 		};
 
-		// Token: 0x040015EA RID: 5610
+		// Token: 0x040015EE RID: 5614
 		private static string s_VersionNumberToken = "VERSION_NUMBER";
 
-		// Token: 0x040015EB RID: 5611
+		// Token: 0x040015EF RID: 5615
 		private static Dictionary<string, string> s_CustomTokens;
 
-		// Token: 0x040015EC RID: 5612
-		private static LocalizationManager.Language s_Language = LocalizationManager.Language.Chinese;
+		// Token: 0x040015F0 RID: 5616
+		private static LocalizationData s_Data;
 
-		// Token: 0x040015ED RID: 5613
-		private static LocalizationData s_Data = null;
+		// Token: 0x040015F5 RID: 5621
+		private static LocalizationLanguage s_Language = null;
 
 		// Token: 0x02000488 RID: 1160
-		public enum Language
-		{
-			// Token: 0x040015F3 RID: 5619
-			English,
-			// Token: 0x040015F4 RID: 5620
-			Dutch,
-			// Token: 0x040015F5 RID: 5621
-			German,
-			// Token: 0x040015F6 RID: 5622
-			Chinese
-		}
-
-		// Token: 0x02000489 RID: 1161
-		public enum LanguageAbbreviation
-		{
-			// Token: 0x040015F8 RID: 5624
-			en,
-			// Token: 0x040015F9 RID: 5625
-			nl,
-			// Token: 0x040015FA RID: 5626
-			de,
-			// Token: 0x040015FB RID: 5627
-			cn
-		}
-
-		// Token: 0x0200048A RID: 1162
-		// (Invoke) Token: 0x06001A56 RID: 6742
+		// (Invoke) Token: 0x06001A67 RID: 6759
 		public delegate void LocalizationLoadedDelegate(bool success);
 
-		// Token: 0x0200048B RID: 1163
-		// (Invoke) Token: 0x06001A5A RID: 6746
-		public delegate void LanguageDelegate(LocalizationManager.Language language);
+		// Token: 0x02000489 RID: 1161
+		// (Invoke) Token: 0x06001A6C RID: 6764
+		public delegate void LanguageDelegate(LocalizationLanguage language);
 
-		// Token: 0x0200048C RID: 1164
-		// (Invoke) Token: 0x06001A5E RID: 6750
+		// Token: 0x0200048A RID: 1162
+		// (Invoke) Token: 0x06001A6F RID: 6767
 		public delegate void TokenDelegate(string token);
 	}
 }
